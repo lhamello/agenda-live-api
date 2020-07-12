ab2ce1bd2463e249bf883706e086093586a31bb8
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-07-12 01:43:54 -0300

    criada a toolbar do nosso HomeComponent

--- curso-angular-9/agendalive-app/src/app/app.module.ts
+++ curso-angular-9/agendalive-app/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
-
 
 @NgModule({
   declarations: [
@@ -16,6 +17,8 @@ import { HomeComponent } from './views/home/home.component';
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
     MatToolbarModule
   ],
   providers: [],
